bf27386e5e4c341f9c49f031b15e1c0673ae1a0b
bdivjan1 <bdivjan1@etf.unsa.ba>
2018-05-15 01:49:35 +0200

    nove forme za meni i mvvm patern

--- Projekat/Projekat/MeniGost.xaml.cs
+++ Projekat/Projekat/MeniGost.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Projekat
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MeniGost : Page
     {
-        public MainPage()
+        public MeniGost()
         {
             this.InitializeComponent();
-        }
-
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- Projekat/Projekat/Models/Korisnik.cs
+++ Projekat/Projekat/Models/Korisnik.cs
@@ -8,6 +8,27 @@ namespace Projekat.Models
 {
     public class Korisnik
     {
+        string ime;
+        string prezime;
+        string korisnickoIme;
+        string email;
+        string lozinka;
 
+        public Korisnik() { }
+
+        public Korisnik(string ime, string prezime, string korisnickoIme, string email, string lozinka)
+        {
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.KorisnickoIme = korisnickoIme;
+            this.Email = email;
+            this.Lozinka = lozinka;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string KorisnickoIme { get => korisnickoIme; set => korisnickoIme = value; }
+        public string Email { get => email; set => email = value; }
+        public string Lozinka { get => lozinka; set => lozinka = value; }
     }
 }
