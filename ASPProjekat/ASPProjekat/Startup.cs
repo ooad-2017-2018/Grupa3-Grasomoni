e82441cd75bfc29c06083c2bb3056507872da9a3
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-06-01 10:34:49 +0200

    ASP kreiran projekat

---
+++ ASPProjekat/ASPProjekat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPProjekat.Startup))]
+namespace ASPProjekat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
