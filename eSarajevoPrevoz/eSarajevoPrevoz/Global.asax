975b197a100f250643d01f1ad357b9f85720c2c4
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-06-02 01:11:01 +0200

    Proba

---
+++ eSarajevoPrevoz/eSarajevoPrevoz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eSarajevoPrevoz.MvcApplication" Language="C#" %>
---
+++ eSarajevoPrevoz/eSarajevoPrevoz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eSarajevoPrevoz.Startup))]
+namespace eSarajevoPrevoz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
